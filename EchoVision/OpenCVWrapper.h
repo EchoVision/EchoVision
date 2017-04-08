935a59c0516a956c518fe787639a2a73bcb09e45
Taranvir Waraich <taranvir@me.com>
2017-04-08 18:05:23 -0400

    Added bridge headers

---
+++ EchoVision/OpenCVWrapper.h
@@ -0,0 +1,13 @@
+//
+//  OpenCVWrapper.h
+//  EchoVision
+//
+//  Created by Taranvir Waraich on 4/8/17.
+//  Copyright © 2017 EchoVision. All rights reserved.
+//
+
+#ifndef OpenCVWrapper_h
+#define OpenCVWrapper_h
+
+
+#endif /* OpenCVWrapper_h */
---
+++ EchoVision/OpenCVWrapper.mm
@@ -0,0 +1,9 @@
+//
+//  OpenCVWrapper.m
+//  EchoVision
+//
+//  Created by Taranvir Waraich on 4/8/17.
+//  Copyright © 2017 EchoVision. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
